dc79a353f164f62478ce3ce41c7b46d8b109fa75
Aaron-Acheampong <aaron.acheampong@amalitech.org>
2022-04-15 15:15:25 +0000

    first front end commit

--- server.js
+++ server.js
@@ -1,21 +1,40 @@
 // JS script here
-var express = require('express');
-var app = express();
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const { urlencoded } = require('body-parser');
 
 // set the view engine to ejs
+app.set('views', './public');
 app.set('view engine', 'ejs');
+app.use(express.static( `public`));
 
+const urlencodedParser = bodyParser.urlencoded({ extended : true });
 // use res.render to load up an ejs view file
 
+// login page
+app.get('', function(req, res) {
+  res.render('Login');
+});
+
+app.get('/Register', function(req, res) {
+    res.render('Register');
+  });
+
+  app.get('/Login', function(req, res) {
+    res.render('Login');
+  });
+
 // index page
-app.get('/', function(req, res) {
-  res.render('pages/index');
+app.post('/login', function(req, res) {
+  res.render('index');
 });
 
-// about page
-app.get('/about', function(req, res) {
-  res.render('pages/about');
-});
+// index page
+app.post('/register', function(req, res) {
+    res.render('Register');
+  });
 
-app.listen(8080);
-console.log('Server is listening on port 8080');
+app.listen(8080, () => {
+    console.log('Server is listening on port 8080')
+ });
