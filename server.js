327b77d6e0618361adf4ba1d2f1f9136bbb74f4c
Aaron-Acheampong <aaron.acheampong@amalitech.org>
2022-04-18 18:03:07 +0000

    New Branch first commit

--- server.js
+++ server.js
@@ -2,15 +2,31 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const { urlencoded } = require('body-parser');
+const { extname, resolve } = require('path');
+
+//imports
+const multer = require('multer');
+const { check, validationResult } = require('express-validator');
+const uuid = require('uuid').v4;
 
 // set the view engine to ejs
+app.use(express.static( __dirname + `/public`));
 app.set('views', './public');
 app.set('view engine', 'ejs');
-app.use(express.static( `public`));
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
-const urlencodedParser = bodyParser.urlencoded({ extended : true });
-// use res.render to load up an ejs view file
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) =>{
+    cb(null, './uploads/');
+  },
+  filename: (req, file, cb) => {
+    const { originalname } = file;
+    cb(null, `${uuid()}-${originalname}`);
+  }
+});
+const upload = multer({ storage: storage });
+
 
 // login page
 app.get('', function(req, res) {
@@ -25,14 +41,78 @@ app.get('/Register', function(req, res) {
     res.render('Login');
   });
 
-// index page
-app.post('/login', function(req, res) {
-  res.render('index');
-});
+  app.get('/FeedPage', function(req, res) {
+    res.render('feedPage');
+  });
+
+  app.get('/Index', function(req, res) {
+    res.render('index');
+  });
 
 // index page
-app.post('/register', function(req, res) {
-    res.render('Register');
+/*
+app.post('/login', function(req, res) {
+  res.redirect('/Index');
+}); */
+
+app.post('/login', urlencodedParser, [
+  check('username', 'This username must be 3+ characters long')
+      .exists()
+      .isLength({ min: 3 }),
+  check('email', 'Email is not valid')
+      .isEmpty()
+      .isEmail()
+      .normalizeEmail(),
+  check('password', 'Invalid password').isEmpty().isLength({ min: 7 })
+
+], (req, res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+      //return res.status(422).jsonp(errors.array())
+      const alert = errors.array();
+
+      res.render('Login', {alert});
+    }
+    //res.redirect('/Register');
+    res.json(req.body);
+  });
+
+
+app.post('/register', urlencodedParser, [
+  check('username', 'This username must be 3+ characters long')
+      .exists()
+      .isLength({ min: 3 }),
+  check('email', 'Email is not valid')
+      .isEmpty()
+      .isEmail()
+      .normalizeEmail(),
+  check('password', 'Invalid password').isEmpty().isLength({ min: 7 })
+
+], (req, res) => {
+    const errors = validationResult(req)
+    if(!errors.isEmpty()) {
+      //return res.status(422).jsonp(errors.array())
+      const alert = errors.array();
+
+      res.render('Register', {alert});
+    }
+    //res.redirect('/Register');git remote -v
+    
+    res.json(req.body);
+  });
+
+  app.post('/upload', upload.single('upload'), (req, res) => {
+     // res.redirect('/Index');
+     //console.log(req);
+     //console.log(req.body);
+     //console.log(req.file);
+     res.json(req.body);
+    });
+
+
+
+  app.get('/search', function(req, res) {
+    res.render('index');
   });
 
 app.listen(8080, () => {
