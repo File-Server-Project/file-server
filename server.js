f586f01580832ee50f1a9dfaf5a4c56c660193ad
Isaac-Ampomah-12 <isaac.ampomah@amalitech.org>
2022-04-21 01:20:55 +0000

    Created a middle ware for forgot password

--- server.js
+++ server.js
@@ -4,6 +4,8 @@ const app = express();
 const bodyParser = require('body-parser');
 const { extname, resolve } = require('path');
 
+const nodemailer = require("nodemailer");
+
 app.use(express.json())
 //  Import dbConnection
 const db = require('./dbConnection');
@@ -167,13 +169,59 @@ app.post('/register', urlencodedParser, [
     check('email', 'Email is not valid')
         .isEmail(),
   
-  ], (req, res) => {
+  ], async (req, res) => {
       const errors = validationResult(req)
       if(!errors.isEmpty()) {
         //return res.status(422).jsonp(errors.array())
         const alert = errors.array();
-  
-        res.render('ForgetPassword', {alert});
+        
+        const {email} = req.body;
+
+        const query = `SELECT * FROM users WHERE email LIKE "%${email}%"`;
+
+        await db.all(query, async (err, rows) => {
+          if(err) return console.error(err.message);
+          console.log(rows.length);
+          if (rows.length === 0){
+              res.json("Invalid user");
+              console.log("Invalid User");
+          }else{
+              // res.json(rows);
+              // console.log(rows);
+
+              let transporter = nodemailer.createTransport({
+                host: "smtp.ethereal.email",
+                port: 587,
+                secure: false, // true for 465, false for other ports
+                auth: {
+                  user: 'gardner.littel10@ethereal.email', // generated ethereal user
+                  pass: 'wuAQHdZaRu7Q7XcBqv', // generated ethereal password
+                },
+              });
+            
+              const msg = {
+                from: '"The File Server" <info@fileserver.com>', // sender address
+                to: `${email}`, // list of receivers
+                subject: "Reset Password", // Subject line
+                text: "Click this link to reset your password", // plain text body
+                html: '<p>Highlight the link below and click "go to to http://localhost:8080/ResetPassword" to reset your password</p> </br> <a>http://localhost:8080/ResetPassword</a>', // html body
+              };
+              // send mail with defined transport object
+              let info = await transporter.sendMail(msg);
+            
+              console.log("Message sent: %s", info.messageId);
+              // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+            
+              // Preview only available when sending through an Ethereal account
+              console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+        
+              console.log('Email Sent');
+              res.json('Email sent');
+              // res.render('index');
+          }
+      });
+
+        // res.render('ForgetPassword', {alert});
       }
   });
 
@@ -183,7 +231,7 @@ app.post('/register', urlencodedParser, [
 
   app.get('/search', async function(req, res) {
     const {search} = req.body;
-
+    console.log(search);
     const query = `SELECT * FROM files WHERE title LIKE "%${search}%" AND description LIKE "%${search}%" `;
 
     await db.all(query, (err, rows) => {
