fcb9d400d361bd93cc2aaa6bfb03d9690e5925b7
Isaac-Ampomah-12 <isaac.ampomah@amalitech.org>
2022-04-28 11:58:15 +0000

    updated the signUp middleware

--- server/server.js
+++ server/server.js
@@ -26,77 +26,11 @@ app.use(uploadLib());
 app.use(express.json());  // accept data in json format
 app.use(express.urlencoded({ extended: true})); // decode data from the HTML form
 
-//imports
-// const multer = require('multer');
-// const { check, validationResult } = require('express-validator');
-// const { timeLog } = require('console');
-// const uuid = require('uuid').v4;
-
-// set the view engine to ejs
-// app.use(express.static( __dirname + `/public`));
-// app.set('views', './public');
-// app.set('view engine', 'ejs');
-// const urlencodedParser = bodyParser.urlencoded({ extended: false });
-
-
-// const storage = multer.diskStorage({
-//   destination: (req, file, cb) =>{
-//     cb(null, './uploads/');
-//   },
-//   filename: (req, file, cb) => {
-//     const { originalname } = file;
-//     cb(null, `${uuid()}-${originalname}`);
-//   }
-// });
-// const upload = multer({ storage: storage });
-
-
-// login page
-// app.get('', function(req, res) {
-//   res.render('Login');
-// });
-
-// app.get('/Register', function(req, res) {
-//     res.render('Register');
-//   });
-
-//   app.get('/Login', function(req, res) {
-//     res.render('Login');
-//   });
-
-//   app.get('/FeedPage', function(req, res) {
-//     res.render('feedPage');
-//   });
-
-//   app.get('/Index', function(req, res) {
-//     res.render('index');
-//   });
-
-//   app.get('/ForgetPassword', function(req, res) {
-//     res.render('ForgetPassword');
-//   });
-
-// index page
-/*
-app.post('/login', function(req, res) {
-  res.redirect('/Index');
-}); */
-
-
 // Users Section
 
 // Login
 app.post('/login', 
-// urlencodedParser, 
-// [
-//   check('username', 'This username must be 3+ characters long')
-//       .exists()
-//       .isLength({ min: 3 }),
-//   check('email', 'Email is not valid')
-//       .isEmail(),
-//   check('password', 'Invalid password').isLength({ min: 7 })
 
-// ], 
 async (req, res) => {
     const {email, password} = req.body;
 
@@ -106,22 +40,36 @@ async (req, res) => {
 
     await db.all(query, (err, rows) => {
       if(err) return console.error(err.message);
-
-      // if (rows.length === 0){
-          // res.json("Invalid user");
-      //     console.log("Invalid User");
-      // }else{
-      //     // res.json(rows);
           console.log(rows);
           res.json(rows);
-          // res.render('index');
-      // }
+
   });
 
 });
 
 
+// Sign UP
+app.post('/signUp', async (req, res) => {
+    const {username, email, password} = req.body;
+    console.log(username, email, password);
+    const query = 'INSERT INTO users (username, email, password) VALUES (?, ?, ?)';
+   
+    await db.run(
+        query,
+        [username, email, password],
+        async (err) => {
+            if(err) return console.error(err.message);
 
+            console.log("User added successfully");
+            res.json("User added successfully");
+            
+        }
+    );
+
+
+  });
+
+  
 
 
 app.listen(port, () => {
